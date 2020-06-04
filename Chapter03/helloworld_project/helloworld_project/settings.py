c405f9e49e7cebb550c393b36547c03542497b65
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-04 22:27:57 +0530

    Heroku deployment setup

--- Chapter03/helloworld_project/helloworld_project/settings.py
+++ Chapter03/helloworld_project/helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l27h(18rlsb4$)yfaym*yrffi1g=$@t=rmjb)d(t)7+4@fv453'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
