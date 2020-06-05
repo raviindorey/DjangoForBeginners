42667c9bd0a1bb6d412b004d486f1d5615228d57
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-06 00:55:59 +0530

    Add posts in blog

--- Chapter05/blog_project/blog/views.py
+++ Chapter05/blog_project/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'all_posts_list'
+    template_name = 'home.html'
