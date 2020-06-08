5dc65bf53e89dcfd5a97fe4de97c01992d0333a9
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-08 19:27:33 +0530

    Add DetailView for post detail

--- Chapter05/blog_project/blog/views.py
+++ Chapter05/blog_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,3 +8,6 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
