c2f3cf3b9f6d4d6ac5d8b165ae775f9a278ca2ce
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-08 20:05:31 +0530

    Initialize from Chapter05

---
+++ Chapter06/blog_project/blog/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'all_posts_list'
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
