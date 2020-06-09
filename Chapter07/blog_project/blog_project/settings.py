0dfb98750defa9eb22e8538ea719b4f3dcae8fe5
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-09 17:37:12 +0530

    Heroku ready

--- Chapter07/blog_project/blog_project/settings.py
+++ Chapter07/blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xkgp5je1lnsvba-lnrymc^ggww5@5uqq21%0v=)vatc6c%_kyw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
