dde50b1d1114e2202cad5925021b4b551fcfeb74
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-11 19:33:02 +0530

    Tests
    - Test HomePageView
    - Test Signup

--- Chapter10/newspaper_project/pages/tests.py
+++ Chapter10/newspaper_project/pages/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class SignupPagesTest(TestCase):
+    username = 'newuser'
+    email = 'newuser@gmail.com'
+
+    def test_signup_page_status_code(self):
+        response = self.client.get('/users/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signup.html')
+
+    def test_signup_form(self):
+        get_user_model().objects.create_user(self.username, self.email)
+        all_users_qs = get_user_model().objects.all()
+        self.assertEqual(all_users_qs.count(), 1)
+        self.assertEqual(all_users_qs[0].username, self.username)
+        self.assertEqual(all_users_qs[0].email, self.email)
