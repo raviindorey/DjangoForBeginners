96acf3b05b388a7b8cff8a0f460821dfa42bc50d
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-12 18:13:30 +0530

    Initialize articles app with model

--- Chapter13/newspaper_project/newspaper_project/settings.py
+++ Chapter13/newspaper_project/newspaper_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Calcutta'
 
 USE_I18N = True
 
