14d7009a72ddc62bd6775ae31e1a379da7e3fd60
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-12 21:34:13 +0530

    Authentication required to CRUD

--- Chapter14/newspaper_project/articles/views.py
+++ Chapter14/newspaper_project/articles/views.py
@@ -1,34 +1,40 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     context_object_name = 'articles'
     template_name = 'article_list.html'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
-    fields = ['author', 'title', 'body']
+    fields = ['title', 'body']
+    login_url = 'login'
     template_name = 'article_new.html'
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class ArticleDetailView(DetailView):
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ['title', 'body']
     template_name = 'article_edit.html'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
