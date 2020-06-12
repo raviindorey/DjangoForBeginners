2dfef196b50b986eac94a8e6021da3aac13c61ca
Ravi Indorey <ravi.indorey90@gmail.com>
2020-06-12 22:05:56 +0530

    Comment TabularInline

--- Chapter15/newspaper_project/articles/admin.py
+++ Chapter15/newspaper_project/articles/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Article, Comment
 
 
-class CommentInline(admin.StackedInline):
+class CommentInline(admin.TabularInline):
     model = Comment
     extra = 1
 
